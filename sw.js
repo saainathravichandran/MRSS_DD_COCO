ed11bf8fb1f2520d294318b82405c37f6e507a4f
saainathravichandran <saainathravichandran@gmail.com>
2026-01-19 22:14:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'mrss-dd-coco-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/mrss-dd-coco/',
+  '/mrss-dd-coco/index.html',
+  '/mrss-dd-coco/manifest.json',
+  '/mrss-dd-coco/icon-192.png',
+  '/mrss-dd-coco/icon-512.png'
 ];
 
 // Install service worker and cache files
